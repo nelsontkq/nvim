5a27e9320017413bee6c673f6ee6661555b88ab9
nelson truran <nelson@truran.dev>
2024-12-24 00:29:53 -0800

    eagerload treesitter

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,4 @@
 local M = {}
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
 
 local function close_buffer_with_prompt()
     local current_buf = vim.api.nvim_get_current_buf()
@@ -9,10 +7,11 @@ local function close_buffer_with_prompt()
     if vim.bo[current_buf].modified then
         if bufname == "" then
             -- Unnamed buffer with changes
-            local choice = vim.fn.confirm("Save?",
-                "&Save As\n&Discard\n&Cancel", 1)
+            local choice = vim.fn.confirm("Save?", "&Save As\n&Discard\n&Cancel", 1)
             if choice == 1 then -- Save As
-                vim.ui.input({ prompt = "Enter file name: " }, function(input)
+                vim.ui.input({
+                    prompt = "Enter file name: "
+                }, function(input)
                     if input then
                         vim.cmd('write ' .. input)
                         vim.cmd('BufferClose')
@@ -41,6 +40,11 @@ local function close_buffer_with_prompt()
 end
 
 function M.setup()
+    local map = vim.keymap.set
+    local opts = {
+        noremap = true,
+        silent = true
+    }
     -- Tab management (barbar)
     map('n', '<leader>p', '<Cmd>BufferPin<CR>', opts)
     map('n', '<tab>', '<Cmd>BufferNext<CR>', opts)
@@ -48,6 +52,29 @@ function M.setup()
     map('n', '<leader>x', close_buffer_with_prompt, opts)
     map('n', '<S-x>', '<Cmd>BufferRestore<CR>', opts)
     map('n', '<leader>kw', '<Cmd>BufferCloseAllButPinned<CR>', opts)
+
+    -- Navigation
+    map('n', '<C-h>', '<C-w>h', opts)
+    map('n', '<C-j>', '<C-w>j', opts)
+    map('n', '<C-k>', "<C-w>k", opts)
+    map('n', '<C-l>', "<C-w>l", opts)
+
+    -- save
+    map('n', "<C-s>", "<cmd> w <CR>", opts)
+
+    -- LSP keybindings
+    map('n', '<leader>fm', function()
+        vim.lsp.buf.format {
+            async = true
+        }
+    end, opts)
+    map('n', '<leader>gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    map('n', '<leader>gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    map('n', '<leader>gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+    map('n', '<leader>gh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    map('n', '<leader>rr', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    map('n', '<leader>.', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+
 
     -- Telescope keybindings
     map('n', '<leader>ff', '<Cmd>Telescope find_files<CR>', opts)
@@ -57,10 +84,15 @@ function M.setup()
     -- NvimTree keybinding
     map('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
 
-
-    -- comment remaps
-    map('n', '<leader>/', 'gcc', { desc = "toggle comment", remap = true })
-    map('v', '<leader>/', 'gc', { desc = "toggle comment", remap = true })
+    -- comment
+    map('n', '<leader>/', 'gcc', {
+        desc = "toggle comment",
+        remap = true
+    })
+    map('v', '<leader>/', 'gc', {
+        desc = "toggle comment",
+        remap = true
+    })
 
     -- which key
     map('n', '<leader>km', function()
